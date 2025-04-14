d7ccca3686798ebbd0b7f2bd208733113cf238db
dai_m <d.matsuda@etbs.jp>
2025-04-15 01:56:46 +0900

    Ver 0.1.4

--- woo-modal-block.php
+++ woo-modal-block.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Woo Modal Block
  * Description:       モーダルウィンドウから商品をカートへ追加できるプラグインです。
- * Version:           0.1.2
+ * Version:           0.1.4
  * Requires at least: 6.7
  * Requires PHP:      7.4
  * Author: DAI
@@ -14,7 +14,7 @@
  *
  * @package CreateBlock
  */
-$woomb_version = '0.1.2';
+$woomb_version = '0.1.4';
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
